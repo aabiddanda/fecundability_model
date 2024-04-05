cece9e047e5b9a2ec6aa47c8d6ab0eb8d71c895a
Arjun Biddanda <aabiddanda@gmail.com>
2024-04-05 16:00:24 -0400

    doc: updated documentation for fecundability model

--- src/fecundability.py
+++ src/fecundability.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class FecundabilityEstimator:
-    """Create an estimator of """
+    """Create an estimator of"""
 
     def __init__(self):
         pass
@@ -10,12 +10,12 @@ class FecundabilityEstimator:
     def p_aneuploid(self):
         """Return the probability of a specific aneuploidy type.
 
-        Entries correspond to baseline probability of: 
+        Entries correspond to baseline probability of:
         * meiotic origin
         * tripolar-mitotic
         * low mosaic
         * high mosaic
-        * euploid 
+        * euploid
         """
         return np.array([0.4, 0.05, 0.1, 0.05, 0.4])
 
@@ -24,7 +24,7 @@ class FecundabilityEstimator:
 
         Entries correspond to the probability of a failed implantation conditional on:
         * meiotic origin aneuploidy
-        * tripolar-mitotic 
+        * tripolar-mitotic
         * low mosaic
         * high mosaic
         * euploid
@@ -36,17 +36,17 @@ class FecundabilityEstimator:
 
         The  probability of an early-pregnancy loss conditional on implantation succeeding
 
-        The output vector corresponds to the probability: 
+        The output vector corresponds to the probability:
         * meiotic origin aneuploidy
-        * tripolar-mitotic 
+        * tripolar-mitotic
         * low mosaic
         * high mosaic
         * euploid
 
         Params:
-            * eta (`float`): This is the "escape probability" of a meiotic or tripolar mitotic aneuploidy. 
-            * eps (`float`): This is the probability of euploid embryo EPL 
+            * eta (`float`): This is the "escape probability" of a meiotic or tripolar mitotic aneuploidy.
+            * eps (`float`): This is the probability of euploid embryo EPL
         """
         assert eta < 1.0
         assert eps < 1.0
-        return np.array([1.-eta, 1.-eta, 0.2, 0.5, eps])
+        return np.array([1.0 - eta, 1.0 - eta, 0.2, 0.5, eps])
