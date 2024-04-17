a92740f19afad3a892fcc8d9f874be9523314de7
Arjun Biddanda <aabiddanda@gmail.com>
2024-04-17 09:19:21 -0400

    feat: added in some plotting routines

--- src/fecundability.py
+++ src/fecundability.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class FecundabilityEstimator:
-    """Create an estimator of"""
+    """Estimators for the conditional probabilities involved in pregnancy losses."""
 
     def __init__(self):
         pass
@@ -50,3 +51,25 @@ class FecundabilityEstimator:
         assert eta < 1.0
         assert eps < 1.0
         return np.array([1.0 - eta, 1.0 - eta, 0.2, 0.5, eps])
+
+
+class FecundabilityPlotting:
+    def __init__():
+        pass
+
+    def plot_macklon_pyramid(ax, xs=np.array([0.3, 0.3, 0.1, 0.3]), **kwargs):
+        """Plot a version of the Macklon et al pyramid."""
+        assert xs.size == 4
+        assert np.isclose(xs.sum(), 1.0)
+        ax.plot([0, 0.5], [0, 1], color="black")
+        ax.plot([0.5, 1], [1, 0], color="black")
+        ax.plot([0, 1], [0, 0], color="black")
+        # Divide pyramid into four tiers
+        ax.plot([0.25 / 2, 1.0 - 0.25 / 2], [0.25, 0.25], lw=2, color="black")
+        ax.plot([0.5 / 2, 1.0 - 0.5 / 2], [0.5, 0.5], lw=2, color="black")
+        ax.plot([0.75 / 2, 1.0 - 0.75 / 2], [0.75, 0.75], lw=2, color="black")
+        ax.axhline(0.5, lw=0.5, linestyle="--", color="black")
+        pos = [0.25 / 2, (0.25 + 0.5) / 2, (0.5 + 0.75) / 2, (0.75 + 0.9) / 2]
+        for i, x in enumerate(xs):
+            ax.text(0.5, pos[i], f"{x*100}%", **kwargs)
+        return ax
