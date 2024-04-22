cc87b66c61097c161e99e1ad33e985d0472f747d
Arjun Biddanda <aabiddanda@gmail.com>
2024-04-22 16:20:59 -0400

    feat: updated the broader fecundability model to include a eta/epsilon model as well

--- src/fecundability.py
+++ src/fecundability.py
@@ -68,7 +68,7 @@ class FecundabilityEstimator:
 
         The  probability of an early-pregnancy loss conditional on implantation succeeding
 
-        Entries correspond to the probability of EPL conditional on:
+        Entries correspond to the probability of EPL (biochemical pregnancy) conditional on:
         ```
              | Meiotic | Euploid
         None | X1      |  X2
@@ -81,39 +81,43 @@ class FecundabilityEstimator:
         """
         assert eta < 1.0
         assert eps < 1.0
-        return np.array([[0.57, eps], [1.0 - eta, 1.0 - eta], [0.57, eps]])
+        return np.array([[1.0 - eta, eps], [1.0 - eta, 1.0 - eta], [1.0 - eta, eps]])
 
-    def p_miscarriage(self, X=None):
+    def p_miscarriage(self, eta_m=1e-2, eps_m=1e-1):
         """Probability of miscarriage conditional on not EPL & implantation not failing
 
-        The output vector corresponds to the probability:
-        * euploid
-        * meiotic
-        * mitotic
+        Entries correspond to the probability of Miscarriage conditional on:
+        ```
+             | Meiotic | Euploid
+        None | X1      |  X2
+        Early| X3      |  X4
+        Late | X5      |  X6
+        ```
+        Params:
+            * eta_m (`float`): This is the "escape probability" of a meiotic or tripolar mitotic aneuploidy.
+            * eps_m (`float`): This is the probability of euploid embryo EPL
 
         """
-        if X is None:
-            return np.array([[0.3, 0.3], [0.3, 0.3], [0.3, 0.3]])
-        else:
-            assert np.all((X > 0) & (X < 1.0))
-            return X
+        return np.array(
+            [[1.0 - eta_m, eps_m], [1.0 - eta_m, 1.0 - eta_m], [1.0 - eta_m, eps_m]]
+        )
 
-    def p_implant_marginal(self, A=None, gamma_implant=5e-2):
+    def p_implant_marginal(self, A=None, A_implant=None, gamma_implant=5e-2):
         """Marginal probability of an implantation error."""
         assert A.shape == (3, 2)
         p_implant = gamma_implant + np.sum(
-            self.p_failed_implantation() * self.p_aneuploid(X=A)
+            self.p_failed_implantation(X=A_implant) * self.p_aneuploid(X=A)
         )
         if p_implant > 1.0:
             warnings.warn("Marginal probability of implantation error is > 1!")
         return p_implant
 
-    def p_epl_marginal(self, A=None, gamma_epl=5e-2, gamma_implant=5e-2):
+    def p_epl_marginal(self, A=None, gamma_epl=5e-2, gamma_implant=5e-2, **kwargs):
         """Marginal probability of an early pregnancy loss."""
         assert A.shape == (3, 2)
         assert (gamma_epl > 0) and (gamma_epl < 1.0)
         p_epl = gamma_epl + np.sum(
-            self.p_epl_implantation()
+            self.p_epl_implantation(**kwargs)
             * (1.0 - self.p_failed_implantation() - gamma_implant)
             * self.p_aneuploid(X=A)
         )
@@ -122,7 +126,12 @@ class FecundabilityEstimator:
         return p_epl
 
     def p_miscarriage_marginal(
-        self, A=None, gamma_mis=5e-2, gamma_epl=5e-2, gamma_implant=5e-2
+        self,
+        A=None,
+        A_implant=None,
+        gamma_mis=5e-2,
+        gamma_epl=5e-2,
+        gamma_implant=5e-2, **kwargs,
     ):
         """Marginal probability of miscarriage."""
         assert A.shape == (3, 2)
@@ -130,9 +139,9 @@ class FecundabilityEstimator:
         assert (gamma_epl > 0) and (gamma_epl < 1.0)
         assert (gamma_implant > 0) and (gamma_implant < 1.0)
         p_mis = gamma_mis + np.sum(
-            self.p_miscarriage()
-            * (1.0 - self.p_epl_implantation() - gamma_epl)
-            * (1.0 - self.p_failed_implantation() - gamma_implant)
+            self.p_miscarriage(**kwargs)
+            * (1.0 - self.p_epl_implantation(**kwargs) - gamma_epl)
+            * (1.0 - self.p_failed_implantation(X=A_implant) - gamma_implant)
             * self.p_aneuploid(X=A)
         )
         if p_mis > 1.0:
@@ -157,6 +166,7 @@ class FecundabilityPlotting:
         ax.plot([0.75 / 2, 1.0 - 0.75 / 2], [0.75, 0.75], lw=2, color="black")
         ax.axhline(0.5, lw=0.5, linestyle="--", color="black")
         pos = [0.25 / 2, (0.25 + 0.5) / 2, (0.5 + 0.75) / 2, (0.75 + 0.9) / 2]
+        txts = []
         for i, x in enumerate(xs):
-            ax.text(0.5, pos[i], f"{x*100:0.1f}%", **kwargs)
-        return ax
+            txts.append(ax.text(0.5, pos[i], f"{x*100:0.1f}%", **kwargs))
+        return ax, txts
